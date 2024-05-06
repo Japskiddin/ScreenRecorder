cf1982b6064cad2b8dbab6db460a374df8d8e65b
Nikita <japskiddin@gmail.com>
2024-05-06 16:58:59 +0800

    * Added publishing to Github Packages

--- screenrecorder/build.gradle.kts
+++ screenrecorder/build.gradle.kts
@@ -1,6 +1,10 @@
+import java.io.FileInputStream
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("maven-publish")
 }
 
 android {
@@ -13,8 +17,48 @@ android {
         }
     }
 
+    packaging {
+        jniLibs {
+            excludes += listOf(
+                "**/kotlin/**",
+                "META-INF/androidx.*",
+                "META-INF/proguard/androidx-*"
+            )
+        }
+        resources {
+            excludes += listOf(
+                "/META-INF/*.kotlin_module",
+                "**/kotlin/**",
+                "**/*.txt",
+                "**/*.xml",
+                "**/*.properties",
+                "META-INF/DEPENDENCIES",
+                "META-INF/LICENSE",
+                "META-INF/LICENSE.txt",
+                "META-INF/license.txt",
+                "META-INF/NOTICE",
+                "META-INF/NOTICE.txt",
+                "META-INF/notice.txt",
+                "META-INF/ASL2.0",
+                "META-INF/*.version",
+                "META-INF/androidx.*",
+                "META-INF/proguard/androidx-*"
+            )
+        }
+    }
+
     buildFeatures {
         buildConfig = true
+    }
+
+    buildTypes {
+        val release by getting {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
     }
 
     compileOptions {
@@ -31,4 +75,73 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.core)
     implementation(libs.androidx.core.ktx)
+}
+
+val propertiesName = "github.properties"
+val githubProperties = Properties().apply {
+    if (rootProject.file(propertiesName).exists()) {
+        load(FileInputStream(rootProject.file(propertiesName)))
+    }
+}
+val outputsDirectoryPath = layout.buildDirectory.dir("outputs").get().toString()
+
+publishing {
+    publications {
+        create<MavenPublication>("ScreenRecorder") {
+            groupId = "io.github.japskiddin"
+            artifactId = "screenrecorder"
+            version = libs.versions.libVersionName.get()
+            artifact("${outputsDirectoryPath}/aar/${artifactId}-release.aar")
+        }
+    }
+
+    repositories {
+        maven {
+            name = "GithubPackages"
+            url = uri("https://maven.pkg.github.com/japskiddin/ScreenRecorder")
+            credentials {
+                username = if (githubProperties.hasProperty("gpr.usr")) {
+                    githubProperties.getProperty("gpr.usr")
+                } else {
+                    System.getenv("GPR_USER")
+                }
+                password = if (githubProperties.hasProperty("gpr.key")) {
+                    githubProperties.getProperty("gpr.key")
+                } else {
+                    System.getenv("GPR_API_KEY")
+                }
+            }
+        }
+    }
+}
+
+val sourceFiles = android.sourceSets.getByName("main").java.getSourceFiles()
+
+tasks.register<Javadoc>("withJavadoc") {
+    isFailOnError = false
+    dependsOn(tasks.named("compileDebugSources"), tasks.named("compileReleaseSources"))
+
+    // add Android runtime classpath
+    android.bootClasspath.forEach { classpath += project.fileTree(it) }
+
+    // add classpath for all dependencies
+    android.libraryVariants.forEach { variant ->
+        variant.javaCompileProvider.get().classpath.files.forEach { file ->
+            classpath += project.fileTree(file)
+        }
+    }
+
+    source = sourceFiles
+}
+
+tasks.register<Jar>("withJavadocJar") {
+    archiveClassifier.set("javadoc")
+    dependsOn(tasks.named("withJavadoc"))
+    val destination = tasks.named<Javadoc>("withJavadoc").get().destinationDir
+    from(destination)
+}
+
+tasks.register<Jar>("withSourcesJar") {
+    archiveClassifier.set("sources")
+    from(sourceFiles)
 }
