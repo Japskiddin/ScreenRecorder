9a01dd8c7943ceb8c9e7c0feed21349390003df8
Nikita <japskiddin@gmail.com>
2024-05-21 14:47:31 +0800

    * Adding publish plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
+
 buildscript {
     dependencies {
         classpath(libs.bundletool)
@@ -8,8 +10,31 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.jetbrains.dokka) apply false
+    alias(libs.plugins.gradle.nexus.publish)
 }
 
 tasks.register("clean", Delete::class) {
     delete(layout.buildDirectory)
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+            username.set(
+                gradleLocalProperties(rootDir, providers).getProperty("sonatype.username")
+                    ?: System.getenv("OSSRH_USERNAME")
+            )
+            password.set(
+                gradleLocalProperties(rootDir, providers).getProperty("sonatype.password")
+                    ?: System.getenv("OSSRH_PASSWORD")
+            )
+            stagingProfileId.set(
+                gradleLocalProperties(rootDir, providers).getProperty("sonatype.stagingProfileId")
+                    ?: System.getenv("OSSRH_STAGING_PROFILE_ID")
+            )
+        }
+    }
 }
--- screenrecorder/build.gradle.kts
+++ screenrecorder/build.gradle.kts
@@ -5,7 +5,9 @@ import java.util.Properties
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.jetbrains.dokka)
     id("maven-publish")
+    id("signing")
 }
 
 android {
@@ -86,36 +88,6 @@ val githubProperties = Properties().apply {
 }
 val outputsDirectoryPath = layout.buildDirectory.dir("outputs").get().toString()
 
-publishing {
-    publications {
-        create<MavenPublication>("ScreenRecorder") {
-            groupId = "io.github.japskiddin"
-            artifactId = "screenrecorder"
-            version = libs.versions.libVersionName.get()
-            artifact("${outputsDirectoryPath}/aar/${artifactId}-release.aar")
-        }
-    }
-
-    repositories {
-        maven {
-            name = "GithubPackages"
-            url = uri("https://maven.pkg.github.com/japskiddin/ScreenRecorder")
-            credentials {
-                username = if (githubProperties.hasProperty("gpr.usr")) {
-                    githubProperties.getProperty("gpr.usr")
-                } else {
-                    System.getenv("GPR_USER")
-                }
-                password = if (githubProperties.hasProperty("gpr.key")) {
-                    githubProperties.getProperty("gpr.key")
-                } else {
-                    System.getenv("GPR_API_KEY")
-                }
-            }
-        }
-    }
-}
-
 val sourceFiles = android.sourceSets.getByName("main").java.getSourceFiles()
 
 tasks.register<Javadoc>("withJavadoc") {
@@ -145,4 +117,94 @@ tasks.register<Jar>("withJavadocJar") {
 tasks.register<Jar>("withSourcesJar") {
     archiveClassifier.set("sources")
     from(sourceFiles)
+}
+
+val dokkaOutputDir = layout.buildDirectory.file("dokka")
+tasks.dokkaHtml { outputDirectory.set(file(dokkaOutputDir)) }
+val deleteDokkaOutputDir by tasks.register<Delete>("deleteDokkaOutputDirectory") {
+    delete(
+        dokkaOutputDir
+    )
+}
+val javadocJar = tasks.create<Jar>("javadocJar") {
+    archiveClassifier.set("javadoc")
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    dependsOn(deleteDokkaOutputDir, tasks.dokkaHtml)
+    from(dokkaOutputDir)
+}
+
+group = "io.github.japskiddin"
+version = libs.versions.libVersionName.get()
+
+publishing {
+    publications {
+        create<MavenPublication>("ScreenRecorder") {
+            groupId = "io.github.japskiddin"
+            artifactId = "screenrecorder"
+            version = libs.versions.libVersionName.get()
+            artifact("${outputsDirectoryPath}/aar/${artifactId}-release.aar")
+        }
+        publications.withType<MavenPublication> {
+            artifact(javadocJar)
+
+            pom {
+                name.set("ScreenRecorder")
+                description.set("Library for recording screen inside application.")
+                url.set("https://github.com/Japskiddin/ScreenRecorder")
+
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+
+                issueManagement {
+                    system.set("GitHub Issues")
+                    url.set("https://github.com/Japskiddin/ScreenRecorder/issues")
+                }
+
+                developers {
+                    developer {
+                        id.set("Japskiddin")
+                        name.set("Nikita Lazarev")
+                        email.set("japskiddin@gmail.com")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:git://github.com/Japskiddin/ScreenRecorder.git")
+                    developerConnection.set("scm:git:ssh://github.com/Japskiddin/ScreenRecorder.git")
+                    url.set("https://github.com/Japskiddin/ScreenRecorder")
+                }
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            name = "GithubPackages"
+            url = uri("https://maven.pkg.github.com/japskiddin/ScreenRecorder")
+            credentials {
+                username = if (githubProperties.hasProperty("gpr.usr")) {
+                    githubProperties.getProperty("gpr.usr")
+                } else {
+                    System.getenv("GPR_USER")
+                }
+                password = if (githubProperties.hasProperty("gpr.key")) {
+                    githubProperties.getProperty("gpr.key")
+                } else {
+                    System.getenv("GPR_API_KEY")
+                }
+            }
+        }
+    }
+}
+
+signing {
+    if (project.hasProperty("signing.gnupg.keyName")) {
+        println("Signing lib...")
+        useGpgCmd()
+        sign(publishing.publications)
+    }
 }
